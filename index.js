f33ba239ca91669cc1b406c2f1a62d5661695fb5
Adam Mauch <maucap20@wfu.edu>
2023-05-22 12:13:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let response
+  if (someValue <= 400 && someValue > 0) {
+    response = 'This one is on me!'
+  }
+  else if (someValue > 400 && someValue <= 2000) {
+    response = 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    response = 'No can do.'
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let answer;
+  someCity === 'NYC' ? (answer = 'Ok, sounds good.') : (answer = 'No go.');
+  return answer;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  let reply;
+  switch (someTip) {
+    case 'generous' :
+      reply = 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      reply = 'Thank you.';
+      break;
+    default :
+      reply = 'Bye.';
+      break;
+  }
+  return reply;
 }
